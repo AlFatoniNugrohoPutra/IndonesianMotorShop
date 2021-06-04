7bba2d78f2fdcbc33c462472c19f38cbcc690579
Al Fatoni Nugroho Putra <50481665+AlFatoniNugrohoPutra@users.noreply.github.com>
2021-06-04 16:19:36 +0700

    Menambahkan semua file

---
+++ assets/js/custom.js
@@ -0,0 +1,29 @@
+// //---------------- transparent navbar effect ----------------//
+// window.addEventListener('scroll', function(){
+//             var header = document.querySelector('header');
+//             header.classList.toggle('sticky', window.scrollY > 0);
+//         });
+
+//         function toggleMenu(){
+//             var menuToggle = document.querySelector('.toggle');
+//             var menu = document.querySelector('.menu');
+//             menuToggle.classList.toggle('active')
+//             menu.classList.toggle('active')
+//         }
+
+
+// // brand
+
+
+// $('.logo-slider').slick({
+//   dots: true,
+//   arrow:true,
+//   infinite: true,
+//   // speed: 300,
+//   slidesToShow: 5,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000
+  
+// });
+
